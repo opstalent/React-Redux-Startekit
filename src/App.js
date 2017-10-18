d4041bef5f5fec29b7b40fcaf40cb82c3a6e5d3f
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-10-18 09:37:30 +0200

    Absolute paths handling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Home from './components/Home';
-import store from "./store/store.js";
+import Home from 'components/Home';
+import store from "store/store";
 
 const App = () => (
   <Provider store={store}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { reducers } from "../reducers/index";
-import { sagas } from "../sagas/index";
+
+import { reducers } from "reducers/index";
+import { sagas } from "sagas/index";
 
 // global temporary store -- it's going to be changes soon
 // add the middlewares
