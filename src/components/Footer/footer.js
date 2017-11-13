be6baca40211d473c338a4f6d97c424e1c8734ea
Igor <staniszewski86@gmail.com>
2017-11-13 14:08:28 +0100

    footer -need styles refactor

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -6,14 +6,50 @@ import './footer.scss';
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className="mainFooter">
-      <div className=" attached stackable menu">
-        <div className="ui container">
-          <a className="item">
-            <i className="home icon"></i> FOOTER
-          </a>
+      <div className="ui inverted vertical footer segment mainFooter">
+        <div className="ui center aligned container">
+          <div className="ui stackable inverted divided grid">
+            <div className="three wide column">
+              <h4 className="ui inverted header">Group 1</h4>
+              <div className="ui inverted link list">
+                <a href="#" className="item">Link One</a>
+                <a href="#" className="item">Link Two</a>
+                <a href="#" className="item">Link Three</a>
+                <a href="#" className="item">Link Four</a>
+              </div>
+            </div>
+            <div className="three wide column">
+              <h4 className="ui inverted header">Group 2</h4>
+              <div className="ui inverted link list">
+                <a href="#" className="item">Link One</a>
+                <a href="#" className="item">Link Two</a>
+                <a href="#" className="item">Link Three</a>
+                <a href="#" className="item">Link Four</a>
+              </div>
+            </div>
+            <div className="three wide column">
+              <h4 className="ui inverted header">Group 3</h4>
+              <div className="ui inverted link list">
+                <a href="#" className="item">Link One</a>
+                <a href="#" className="item">Link Two</a>
+                <a href="#" className="item">Link Three</a>
+                <a href="#" className="item">Link Four</a>
+              </div>
+            </div>
+            <div className="seven wide column">
+              <h4 className="ui inverted header">Footer Header</h4>
+              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            </div>
+          </div>
+          <div className="ui inverted section divider"></div>
+          <img src="assets/images/logo.png" className="ui centered mini image"/>
+          <div className="ui horizontal inverted small divided link list">
+            <a className="item" href="#">Site Map</a>
+            <a className="item" href="#">Contact Us</a>
+            <a className="item" href="#">Terms and Conditions</a>
+            <a className="item" href="#">Privacy Policy</a>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
